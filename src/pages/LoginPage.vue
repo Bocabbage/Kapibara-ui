23940c46cfc9d41ef1d93e4d854f98ba2faf0398
Zhuofan Zhang <378066505@qq.com>
2023-10-15 11:49:53 +0800

    Update: add login password digest.

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,33 +1,57 @@
 <script setup>
 import 'tdesign-vue-next/es/style/index.css';
 import { reactive } from 'vue';
-import { MessagePlugin } from 'tdesign-vue-next';
 import { DesktopIcon, LockOnIcon } from 'tdesign-icons-vue-next';
+import { sha256 } from 'js-sha256';
+// bcrypt from 'bcryptjs'
 
-const formData = reactive({
+
+const login_api_uri = import.meta.env.VITE_LOGIN_API_URI
+const login_pwd_salt = import.meta.env.VITE_USER_PASSWORD_SALT
+
+const login_info = reactive({
   account: '',
   password: '',
 });
 
-const onReset = () => {
-  MessagePlugin.success('重置成功');
-};
 
-const onSubmit = ({ validateResult, firstError }) => {
-  if (validateResult === true) {
-    MessagePlugin.success('提交成功');
-  } else {
-    console.log('Validate Errors: ', firstError, validateResult);
-    MessagePlugin.warning(firstError);
+async function onSubmit()
+{
+  // User_password encryption in front-end
+  const salt_rounds = 0
+  // let crypto_password = await bcrypt.hash(sha256(login_info.password), login_pwd_salt)
+  let crypto_password = sha256(login_info.password + login_pwd_salt)
+  // Send form-data of login-info
+  let response = await fetch(
+      login_api_uri,
+      {
+        method: 'POST',
+        body: JSON.stringify(
+            {
+              account: login_info.account,
+              password: crypto_password
+            }
+        ),
+        headers: {
+          'Content-Type': 'application/json'
+        }
+      }
+  )
+  let content = await response.json()
+  if(content.code === 200) {
+    console.log('Login success!')
   }
-};
+  else {
+    console.error('Error code:', content.code)
+  }
+}
 </script>
 
 <template>
   <t-space direction="vertical">
     <div>
       <t-layout class="same-color-bg">
-        <t-header class="same-color-bg" height="150px">Kapibara</t-header>
+        <t-header class="same-color-bg" height="200px">Kapibara</t-header>
         <t-content>
           <t-card hoverShadow bordered>
             <div class="login-form-container">
@@ -42,9 +66,9 @@ const onSubmit = ({ validateResult, firstError }) => {
               </div>
 
               <div class="login-form-item" id="inputform" align="center">
-                <t-form ref="form" :data="formData" :colon="true" :label-width="0" @reset="onReset" @submit="onSubmit">
+                <t-form ref="form" :data="login_info" :colon="true" :label-width="0" @submit="onSubmit">
                   <t-form-item name="account">
-                    <t-input v-model="formData.account" clearable placeholder="Account">
+                    <t-input v-model="login_info.account" clearable placeholder="Account">
                       <template #prefix-icon>
                         <desktop-icon />
                       </template>
@@ -53,36 +77,38 @@ const onSubmit = ({ validateResult, firstError }) => {
                   </t-form-item>
 
                   <t-form-item name="password">
-                    <t-input v-model="formData.password" type="password" clearable placeholder="Password">
+                    <t-input v-model="login_info.password" type="password" clearable placeholder="Password">
                       <template #prefix-icon>
                         <lock-on-icon />
                       </template>
 
                     </t-input>
                   </t-form-item>
+
+                  <div class="login-form-item" align="right">
+                    <t-space size="small">
+                      <t-button
+                          theme="danger"
+                          size="large">
+                        Sign in
+                      </t-button>
+
+                      <t-button
+                          theme="success"
+                          size="large"
+                          type="submit"
+                      >
+                        Log in
+                      </t-button>
+
+                    </t-space>
+                  </div>
                 </t-form>
-              </div>
-
-              <div class="login-form-item" align="right">
-                <t-space size="small">
-                  <t-button
-                      theme="danger"
-                      size="large">
-                    Sign in
-                  </t-button>
-
-                  <t-button
-                      theme="success"
-                      size="large">
-                    Log in
-                  </t-button>
-
-                </t-space>
               </div>
             </div>
           </t-card>
         </t-content>
-        <t-footer height="150px"></t-footer>
+        <t-footer height="200px"></t-footer>
       </t-layout>
     </div>
   </t-space>
