a892f247e839431a813e363eaca03e651026aa38
Bocabbage <378066505@qq.com>
2023-12-26 23:44:47 +0800

    Optimize the loginpage.

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -7,38 +7,32 @@ import LoginForm from '@/components/LoginForm.vue'
 </script>
 
 <template>
-  <t-space direction="vertical">
-    <div>
-      <t-layout class="same-color-bg">
-        <t-header class="same-color-bg" height="200px">Kapibara</t-header>
-        <t-content>
-          <t-card hoverShadow bordered>
-            <div class="login-form-container">
 
-              <div class="login-form-item" id="kapipara-img-div" align="center">
-                <t-image
-                    shape="circle"
-                    id="kapipara-img"
-                    src="Kapibara-icon.jpg"
-                    :style="{ width: '120px', height: '120px' }"
-                />
-              </div>
+  <t-layout class="same-color-bg">
+    <t-header class="same-color-bg login-header" height="200px">Kapibara</t-header>
+    <t-content class="login-content">
+      <t-card class="login-card" hoverShadow bordered>
+        <div class="login-form-container">
 
-              <LoginForm />
-            </div>
-          </t-card>
-        </t-content>
-        <t-footer height="200px"></t-footer>
-      </t-layout>
-    </div>
-  </t-space>
+          <div class="login-form-item" id="kapipara-img-div" align="center">
+            <t-image
+                shape="circle"
+                id="kapipara-img"
+                src="Kapibara-icon.jpg"
+                :style="{ width: '120px', height: '120px' }"
+            />
+          </div>
+
+          <LoginForm />
+        </div>
+      </t-card>
+    </t-content>
+    <t-footer height="80px">Bocabbage</t-footer>
+  </t-layout>
 </template>
 
 <style scoped>
-
-body {
-  background-color: #99D798;
-}
+@import url('https://fonts.googleapis.com/css?family=Oswald');
 
 .same-color-bg {
   background-color: #99D798;
@@ -51,6 +45,26 @@ body {
   /* grid-template-rows: repeat(3, 1fr); */
   grid-gap: 20px;
   gap: 20px;
+}
+
+.login-header {
+  font-family: 'Oswald';
+  font-size: 80px;
+  display: grid;
+  place-items: center;
+}
+
+.login-content {
+  height: 800px;
+  display: grid;
+  align-items: start;
+  justify-items: center;
+}
+
+.login-card {
+  max-width: 400px;
+  display: grid;
+  align-items: center;
 }
 
 </style>
